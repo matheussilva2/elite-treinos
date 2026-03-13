a120447b485bafa15160d784a4b97f955503dd90
Matheus <matheusnascimentoeoq@gmail.com>
2026-03-13 19:30:31 -0300

    feat: created coach model

---
+++ backend/app/Models/Coach.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Coach extends Model
+{
+    protected $fillable = ['phone', 'cref'];
+
+    public function user(): BelongsTo {
+        return $this->belongsTo(User::class);
+    }
+}

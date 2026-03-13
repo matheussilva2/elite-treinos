fe9947d32f6d31f03985086fcc3c28da4be03118
Matheus <matheusnascimentoeoq@gmail.com>
2026-03-13 20:04:40 -0300

    feat: added coach and clientWorkouts relationship methods to Client model

--- backend/app/Models/Client.php
+++ backend/app/Models/Client.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Client extends Model
 {
@@ -11,5 +12,13 @@ class Client extends Model
 
     public function user(): BelongsTo {
         return $this->belongsTo(User::class);
+    }
+
+    public function coach(): BelongsTo {
+        return $this->belongsTo(Coach::class);
+    }
+
+    public function clientWorkouts(): HasMany {
+        return $this->hasMany(ClientWorkout::class);
     }
 }

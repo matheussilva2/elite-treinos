fece0afe653d273757bc2aeb6d49941354e64207
Matheus <matheusnascimentoeoq@gmail.com>
2026-03-13 21:55:07 -0300

    feat: make role middleware for preventing forbidden role to access resources

---
+++ backend/app/Http/Middleware/RoleMiddleware.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response as HttpResponse;
+use Symfony\Component\HttpFoundation\Response;
+
+class RoleMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  Closure(Request): (Response)  $next
+     */
+    public function handle(Request $request, Closure $next, string ...$roles): Response
+    {
+        if(!in_array($request->user()->role, $roles)) {
+            return response()->json([], HttpResponse::HTTP_FORBIDDEN);
+        }
+
+        return $next($request);
+    }
+}

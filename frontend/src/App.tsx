76bdb90bcccc4c721bbe58f1118fe29f96b004e7
Matheus <matheusnascimentoeoq@gmail.com>
2026-03-14 18:26:35 -0300

    feat: implemented private route rule, check role method and index/show coaches routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,53 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 import './App.css'
-import { AuthProvider } from "./contexts/AuthContext"
-import { Login } from "./pages/auth/login"
+import { AuthProvider, useAuth } from './contexts/AuthContext'
+import { Coaches } from "./pages/admin/Coaches/Coaches";
+import { Login } from "./pages/auth/Login";
+import { CoachDetails } from "./pages/admin/Coaches/CoachDetails";
+
+function PrivateRoute({ children, roles }: { children: React.ReactNode, roles: string[] }) {
+  const { user, loading } = useAuth();
+
+  if(loading) return <div>carregando...</div>;
+  if(!user) return <Navigate to='/login' />;
+  if(!roles.includes(user.role)) return <Navigate to='/login' />;
+
+  return <>{children}</>;
+}
+
+function CheckRole() {
+  const { user, loading } = useAuth();
+
+  if(loading) return <div>carregando...</div>;
+  if(!user) return <Navigate to='/login'/>
+
+  if(user.role === 'superadmin') return <Navigate to='/coaches' />;
+  if(user.role === 'coach') return <Navigate to='/clients' />;
+
+  return <Navigate to='/my-workouts' />;
+}
 
 export default function App() {
+
+
   return (
     <BrowserRouter>
       <AuthProvider>
         <Routes>
+          <Route path='/' element={<CheckRole />} />
           <Route path='/login' element={<Login />} /> 
+
+          <Route path='/coaches' element={
+            <PrivateRoute roles={['superadmin']}>
+              <Coaches />
+            </PrivateRoute>
+          } />
+          <Route path='/coaches/:id' element={
+            <PrivateRoute roles={['superadmin']}>
+              <CoachDetails />
+            </PrivateRoute>
+          } />
+
         </Routes>
       </AuthProvider>
     </BrowserRouter>

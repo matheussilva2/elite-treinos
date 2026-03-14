98497cce39197bb07757ba673aaf5fff2ff3d3cb
Matheus <matheusnascimentoeoq@gmail.com>
2026-03-13 23:27:14 -0300

    feat: implemented method for get coach clients

--- backend/app/Models/Coach.php
+++ backend/app/Models/Coach.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Coach extends Model
 {
@@ -14,5 +15,9 @@ class Coach extends Model
     
     public function user(): BelongsTo {
         return $this->belongsTo(User::class, 'user_id', 'id');
+    }
+
+    public function clients(): HasMany {
+        return $this->hasMany(Client::class, 'coach_id', 'user_id');
     }
 }

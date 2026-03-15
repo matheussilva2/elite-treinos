b800a0bc03db91117de9fde848c3834ac34c2f74
Matheus <matheusnascimentoeoq@gmail.com>
2026-03-15 00:44:30 -0300

    feat: created route for get all workouts

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Auth\AuthController;
 use App\Http\Controllers\ClientController;
+use App\Models\Workout;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/auth/login', [AuthController::class, 'login'])->name('auth.login');
@@ -10,6 +11,9 @@ Route::middleware('auth:sanctum')->group(function() {
     Route::post('/auth/logout', [AuthController::class, 'logout'])->name('auth.logout');
     Route::get('/auth/me', [AuthController::class, 'me'])->name('auth.me');
     Route::get('/my-workouts', [ClientController::class, 'myWorkouts'])->middleware('role:client')->name('workouts.my');
+    Route::get('/workouts', function(){
+        return Workout::all()->load('exercises');
+    })->middleware('role:superadmin,coach')->name('workout.index');
 });
 
 require __DIR__ . '/api/coaches.php';
